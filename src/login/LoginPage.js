2949cbb17b08bf355d4a345c9441ee46b2f9f523
Adriano Domingues <54463264+programadormovel@users.noreply.github.com>
2021-11-18 13:01:09 -0300

    corrigido problema de login duplo

--- src/login/LoginPage.js
+++ src/login/LoginPage.js
@@ -12,18 +12,25 @@ import {
 
 // Import the functions you need from the SDKs you need
 // yarn add "firebase/app"
-import { initializeApp } from "@firebase/app";
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth, signInWithEmailAndPassword } from "@firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 // import { AsyncStorage } from "@react-native-async-storage/async-storage";
+import { useForm, Controller } from "react-hook-form";
 
 const LoginPage = ({ navigation }) => {
   const [email, setEmail] = useState("");
-  // const [emailLogado, setEmailLogado] = React.useState();
+
   const [password, setPassword] = useState("");
 
   const [sucesso, setSucesso] = useState(false);
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues: { email: "", password: "" } });
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
@@ -41,43 +48,97 @@ const LoginPage = ({ navigation }) => {
 
   const auth = getAuth();
 
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      // ...
-      //setEmailLogado(user.email);
-      setSucesso(true);
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-      // alert(errorCode);
-      // alert(errorMessage);
-    });
+  const onSubmit = async (data) => {
+    console.log(data);
+
+    // Alterar dados dos estados
+    setEmail(data.email);
+    setPassword(data.password);
+
+    console.log(data.email);
+    // Verificar se login e senha conferem
+    await signInWithEmailAndPassword(auth, data.email, data.password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        //setEmailLogado(user.email);
+        setSucesso(true);
+
+        if (user.email === data.email) {
+          navigation.navigate("Rotas");
+        } else {
+          Alert.alert(
+            "Login não realizado, verifique seu email ou senha ou realize seu cadastro!"
+          );
+        }
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        if (errorCode === "auth/wrong-password") {
+          alert("Wrong password.");
+        } else if (errorCode === "auth/invalid-email") {
+          alert("Email inválido!");
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+      });
+    
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
         <Text>LOGIN FIREBASE</Text>
 
-        <TextInput
-          placeholder="Digite seu e-mail"
-          style={styles.caixa}
-          onChangeText={setEmail}
-          //value={email}
+        <Controller
+          control={control}
+          rules={{ required: true }}
+          name="email"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              placeholder="Digite seu e-mail"
+              style={styles.caixa}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value}
+              textContentType="emailAddress"
+              autoCapitalize="none"
+              keyboardType="email-address"
+            />
+          )}
         />
+        {errors.email && <Text>Entre com o email. Campo Obrigatório.</Text>}
 
-        <TextInput
-          placeholder="Entre com sua senha"
-          style={styles.caixa}
-          onChangeText={setPassword}
-          //value={password}
+        <Controller
+          control={control}
+          rules={{ required: true, minLength: 6 }}
+          name="password"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              placeholder="Entre com sua senha"
+              style={styles.caixa}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value}
+              secureTextEntry={true}
+              textContentType="password"
+            />
+          )}
         />
+        {errors.password && (
+          <Text>
+            Entre com a senha. Mínimo de 6 caracteres. Campo Obrigatório.
+          </Text>
+        )}
 
-        <TouchableOpacity style={{alignSelf:'flex-end'}}
-            onPress={()=>{navigation.navigate('RegistroPage')}}
+        <TouchableOpacity
+          style={{ alignSelf: "flex-end" }}
+          onPress={() => {
+            navigation.navigate("RegistroPage");
+          }}
         >
           <Text>Registrar-se</Text>
         </TouchableOpacity>
@@ -89,15 +150,7 @@ const LoginPage = ({ navigation }) => {
             padding: 8,
             borderRadius: 10,
           }}
-          onPress={() => {
-            signInWithEmailAndPassword(auth, email, password);
-            if (sucesso == true) {
-              alert("Logou " + email);
-              navigation.navigate("Rotas");
-            } else {
-              Alert.alert('Login não realizado, verifique seu email ou senha ou realize seu cadastro!');
-            }
-          }}
+          onPress={handleSubmit(onSubmit)}
         >
           <Text style={{ color: "white", fontWeight: "bold" }}>ENTRAR</Text>
         </TouchableOpacity>
